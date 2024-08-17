1797b7831d440bb24805a481777c56e5b95a3392
AyazAktaş <ayazaktastr@gmail.com>
2024-08-17 15:58:46 +0300

    butonlara fonksiyon kullanımı

--- app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
+++ app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
-
+        binding.mainActivityNesnesi=this
         enableEdgeToEdge()
 
 
