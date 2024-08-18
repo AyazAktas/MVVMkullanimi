13bc6989fbb26d3fbd589b927469a5da4a7e95f3
AyazAktaş <ayazaktastr@gmail.com>
2024-08-18 15:38:35 +0300

    repository and data source

--- app/src/main/java/com/example/mvvmkullanimi/MainActivityViewModel.kt
+++ app/src/main/java/com/example/mvvmkullanimi/MainActivityViewModel.kt
@@ -2,22 +2,23 @@ package com.example.mvvmkullanimi
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivityViewModel: ViewModel() {
 
     var sonuc=MutableLiveData("0")
+    var mrepo=MatematikRepository()
 
     fun toplamaYap(alinanSayi1:String,alinanSayi2:String){
-        val sayi1=alinanSayi1.toInt()
-        val sayi2=alinanSayi2.toInt()
-        val toplam=sayi2+sayi1
-        sonuc.value=toplam.toString()
-
+        CoroutineScope(Dispatchers.Main).launch {
+            sonuc.value=mrepo.toplamaYap(alinanSayi1,alinanSayi2)
+        }
     }
     fun carpmaYap(alinanSayi1:String,alinanSayi2:String){
-        val sayi1=alinanSayi1.toInt()
-        val sayi2=alinanSayi2.toInt()
-        val carpma=sayi2*sayi1
-        sonuc.value=carpma.toString()
+        CoroutineScope(Dispatchers.Main).launch {
+            sonuc.value=mrepo.carpmaYap(alinanSayi1,alinanSayi2)
+        }
     }
 }
